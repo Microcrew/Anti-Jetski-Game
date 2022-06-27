0483c16e42e6cf94f6cc1fd5eabffdb0afbf8349
Karl Wedberg <karl.wedberg@gmail.com>
2022-06-27 18:12:51 +0200

    Made Player rotate around center

--- JetskiHater.py
+++ JetskiHater.py
@@ -15,7 +15,7 @@ class App:
     def __init__(self):
         self.running = True
         self.display = None
-        self.size = self.width, self.height = 640, 400
+        self.size = self.width, self.height = 1280, 800
         
  
     def on_init(self):
@@ -28,11 +28,22 @@ class App:
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             self.running = False
             
-        elif event.type == KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                game_objects[0].turn_right()
-            elif event.key == pygame.K_LEFT:
-                game_objects[0].turn_left()
+        #elif event.type == KEYDOWN:
+            #if event.key == pygame.K_RIGHT:
+                #game_objects[0].turn_right()
+            #elif event.key == pygame.K_LEFT:
+                #game_objects[0].turn_left()
+
+            
+
+    def key_check(self):
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            game_objects[0].turn_right()
+        elif keys[pygame.K_LEFT]:
+            game_objects[0].turn_left()
+        
                 
                 
 
@@ -42,7 +53,7 @@ class App:
 
     
     def on_render(self):
-        self.screen.fill((100,100,100))
+        self.screen.fill((0,94,184))
 
         for gameobject in game_objects:
             gameobject.draw()
@@ -59,7 +70,7 @@ class App:
         if self.on_init() == False:
             self.running = False
 
-        player = Player(theApp.width/2, theApp.height/2, theApp)
+        player = Player((theApp.width/2), (theApp.height/2), theApp)
         
         game_objects.append(player)
 
@@ -68,6 +79,7 @@ class App:
         while( self.running ):
             for event in pygame.event.get():
                 self.on_event(event)
+            self.key_check()
             self.on_loop()
             self.on_render()
         self.on_cleanup()
@@ -80,29 +92,43 @@ class Game_Object:
         self.y_position = y
         self.texture = texture
         self.gamespace = gamespace
+        self.rect = None
 
     def draw(self):
-        theApp.screen.blit(self.texture, (self.x_position,self.y_position))
+        if self.rect == None:
+            theApp.screen.blit(self.texture, (self.x_position,self.y_position))
+        else:
+            theApp.screen.blit(self.texture, self.rect)
+            
         
         
     
 
 
 class Player(Game_Object):
+    global playertexture
     def __init__(self, x, y, gamespace):
         playertexture = pygame.image.load("Duck_for_game.png")
         super().__init__(x,y, playertexture, gamespace)
         
-        self.rotation = 180
+        self.rotation = 0
         self.ammo = 10
         
 
     def turn_right(self):
-        self.rotation += 1
+        self.rotation += 0.3
+        old_texture = self.texture
+        self.texture = pygame.transform.rotate(pygame.image.load("Duck_for_game.png"), self.rotation)
+        new_rect = self.texture.get_rect(center = old_texture.get_rect(center = (self.x_position, self.y_position)).center)
+        self.rect = new_rect
 
     def turn_left(self):
-        self.rotation -= 1
-
+        self.rotation -= 0.3
+        old_texture = self.texture
+        self.texture = pygame.transform.rotate(pygame.image.load("Duck_for_game.png"), self.rotation)
+        new_rect = self.texture.get_rect(center = old_texture.get_rect(center = (self.x_position, self.y_position)).center)
+        self.rect = new_rect
+        
     def shoot(self):
         self.ammo -= 1
         
