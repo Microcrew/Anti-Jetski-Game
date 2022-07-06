70fb7e09ae60270c73a94824f3b75036930cf266
Karl Wedberg <karl.wedberg@gmail.com>
2022-07-06 14:43:43 +0200

    Added shooting and ammo tracking

--- JetskiHater.py
+++ JetskiHater.py
@@ -1,10 +1,17 @@
 import pygame
 from pygame.locals import *
+import math
 
 
 
 
 game_objects = []
+bullets = []
+
+bullet_speed = 0.2
+turn_speed = 0.3
+spawn_margin = 10
+clip_size = 5
 
 
 
@@ -27,6 +34,10 @@ class App:
     def on_event(self, event):
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             self.running = False
+
+        elif (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+            player.shoot()
+
             
         #elif event.type == KEYDOWN:
             #if event.key == pygame.K_RIGHT:
@@ -43,20 +54,37 @@ class App:
             game_objects[0].turn_right()
         elif keys[pygame.K_LEFT]:
             game_objects[0].turn_left()
-        
-                
-                
 
-            
+
+
+    def move_bullets(self):
+        for item in bullets:
+            item.travel()
+            if (item.getX() < -spawn_margin or item.getX() > theApp.width + spawn_margin or item.getY() < -spawn_margin or item.getY() > theApp.height + spawn_margin):
+                game_objects.remove(item)
+                bullets.remove(item)
+                
+                
+                
     def on_loop(self):
         pass
 
     
     def on_render(self):
+
+        #Draw sea
         self.screen.fill((0,94,184))
 
+        
+        #Draw all Game Objects
         for gameobject in game_objects:
             gameobject.draw()
+
+        #Draw UI
+        font = pygame.font.SysFont("helvetica", 40)
+        text2 = font.render("Raketer: " + str(player.get_ammo()), False, (0, 0, 0))
+        theApp.screen.blit(text2,(0,0))
+        
             
         pygame.display.flip()
         
@@ -70,18 +98,30 @@ class App:
         if self.on_init() == False:
             self.running = False
 
+        global player
         player = Player((theApp.width/2), (theApp.height/2), theApp)
         
         game_objects.append(player)
 
         pygame.display.set_caption("JetskiHater")
- 
+        
+        ammo_regen = 0
+        
         while( self.running ):
             for event in pygame.event.get():
                 self.on_event(event)
             self.key_check()
+            self.move_bullets()
+            
+            if ammo_regen == 1000:
+                player.add_ammo()
+                ammo_regen = 0
+                
             self.on_loop()
             self.on_render()
+            
+            ammo_regen += 1
+            
         self.on_cleanup()
         
 
@@ -99,6 +139,18 @@ class Game_Object:
             theApp.screen.blit(self.texture, (self.x_position,self.y_position))
         else:
             theApp.screen.blit(self.texture, self.rect)
+
+    def setX(self, x):
+        self.x_position = x
+
+    def setY(self, y):
+        self.y_position = y
+
+    def getX(self):
+        return self.x_position
+
+    def getY(self):
+        return self.y_position
             
         
         
@@ -112,27 +164,64 @@ class Player(Game_Object):
         super().__init__(x,y, playertexture, gamespace)
         
         self.rotation = 0
-        self.ammo = 10
+        self.ammo = clip_size
         
 
     def turn_right(self):
-        self.rotation += 0.3
+        self.rotation += turn_speed
+        if self.rotation > 360:
+            self.rotation = self.rotation - 360
+
+        elif self.rotation < 0:
+            self.rotation = self.rotation + 360
+            
         old_texture = self.texture
         self.texture = pygame.transform.rotate(pygame.image.load("Duck_for_game.png"), self.rotation)
         new_rect = self.texture.get_rect(center = old_texture.get_rect(center = (self.x_position, self.y_position)).center)
         self.rect = new_rect
 
     def turn_left(self):
-        self.rotation -= 0.3
+        self.rotation -= turn_speed
+        if self.rotation > 360:
+            self.rotation = self.rotation - 360
+
+        elif self.rotation < 0:
+            self.rotation = self.rotation + 360
+            
         old_texture = self.texture
         self.texture = pygame.transform.rotate(pygame.image.load("Duck_for_game.png"), self.rotation)
         new_rect = self.texture.get_rect(center = old_texture.get_rect(center = (self.x_position, self.y_position)).center)
         self.rect = new_rect
         
     def shoot(self):
-        self.ammo -= 1
+        if self.ammo > 0:
+            self.ammo -= 1
+            new_bullet = Bullet(self.x_position, self.y_position, self.gamespace, self.rotation)
+            bullets.append(new_bullet)
+            game_objects.append(new_bullet)
+
+    def add_ammo(self):
+        if self.ammo < clip_size:
+            self.ammo += 1
+
+    def get_ammo(self):
+        return self.ammo
         
 
+class Bullet(Game_Object):
+    def __init__(self, x, y, gamespace, direction):
+        texture = pygame.image.load("Bullet.png")
+        self.speed = bullet_speed
+        self.direction = direction
+
+        self.bull_array_placement = 0
+        self.game_array_placement = 0
+        super().__init__(x,y,texture,gamespace)
+        
+    def travel(self):
+        super().setX(self.x_position - self.speed*(math.sin(math.radians(self.direction))))
+        super().setY(self.y_position - self.speed*(math.cos(math.radians(self.direction))))
+        
 
 
 
